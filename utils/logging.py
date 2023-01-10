bb785334dc6e2c2ed06269b7139ff7a017d24fb9
m-altieri <massimiliano.altieri@uniba.it>
2023-01-10 09:49:03 -0500

    .

--- utils/logging.py
+++ utils/logging.py
@@ -1,8 +1,11 @@
 import logging
 import sys
-
+import os
 
 def create_logger(log_file_path):
+    
+    is not os.path.exists(os.path.dirname(log_file_path)):
+        os.makedirs(os.path.dirname(log_file_path))
     log_file = open(log_file_path, 'w')
     logger = logging.getLogger()
     logger.setLevel('DEBUG')
