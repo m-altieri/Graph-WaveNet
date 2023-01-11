7b21388754b496ec731fd1a62bdd2952d5fd101a
m-altieri <massimiliano.altieri@uniba.it>
2023-01-11 03:58:29 -0500

    .

--- exp-runner-pytorch.py
+++ exp-runner-pytorch.py
@@ -230,8 +230,8 @@ def train_and_predict(model, trainX, trainY, valX, valY, testX, testY, test_inde
 
         dataset = torch.utils.data.TensorDataset(torch.Tensor(trainX[start_index : index]), torch.Tensor(trainY[start_index : index]))
         training_loader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=False, num_workers=1)
-        epochs = 10  # args.epochs
-        for epoch in range(epochs):
+        
+        for epoch in range(args.epochs):
             train_loss = []
             for i, data in enumerate(training_loader):
 
@@ -257,7 +257,7 @@ def train_and_predict(model, trainX, trainY, valX, valY, testX, testY, test_inde
                 #torch.nn.utils.clip_grad_norm_(model.parameters(), 5)  # clip = 5
                 optimizer.step()
                 train_loss.append(mae.detach().cpu().numpy())
-            logger.info(f'Epochs: {epoch}/{epochs}  (MAE: {np.mean(train_loss):.4f})')
+            logger.info(f'Epochs: {epoch+1}/{epochs}  (MAE: {np.mean(train_loss):.4f})')
 
         logger.info(f'Predicting on {np.expand_dims(np.transpose(testX[i], (2,1,0)), 0).shape}')
         pred = model(torch.Tensor(np.expand_dims(np.transpose(testX[i], (2,1,0)), 0)).to(device))
